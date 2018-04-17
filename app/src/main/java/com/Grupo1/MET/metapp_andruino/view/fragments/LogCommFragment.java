cd9bf06e97f6b06f7c9f7add58d3a821a8164063
Paulins <ls27128@salleurl.edu>
2018-04-17 11:42:01 +0200

    Fixed bug that caused logs to show up as white text on some phones.

--- app/src/main/java/com/Grupo1/MET/metapp_andruino/view/fragments/LogCommFragment.java
+++ app/src/main/java/com/Grupo1/MET/metapp_andruino/view/fragments/LogCommFragment.java
@@ -56,7 +56,7 @@ public class LogCommFragment extends Fragment {
         ListView lvLogs = v.findViewById(R.id.lvLogComm);
         List<String> logs = Logger.getInstance(getActivity().getApplicationContext()).getLogs();
         Collections.reverse(logs);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, logs.toArray(new String[0]));
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, logs.toArray(new String[0]));
         lvLogs.setAdapter(adapter);
         ImageButton ibBack = v.findViewById(R.id.image_button_back_log_comm);
         ibBack.setOnClickListener(new View.OnClickListener() {
